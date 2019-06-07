b91697acedc734d19837f3f3b0a3be4471525963
3057 <49687272+3057@users.noreply.github.com>
2019-06-07 22:29:35 +0800

    Add files via upload

---
+++ get_voice.py
@@ -0,0 +1,40 @@
+import pyaudio
+import wave
+import time
+
+def get_voice():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 48000
+    RECORD_SECONDS = 3
+    WAVE_OUTPUT_FILENAME = "guess/wav/unknown-common-1.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+    time.sleep(0.5)
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
